0e31382032e725cdc3b144e717ba7bc47eacda19
Dru Sellers <dru@drusellers.com>
2010-07-07 15:03:12 -0500

    moving the EndpointFactory to the right spot

--- lib/masstransit.rb
+++ lib/masstransit.rb
@@ -1,2 +1,3 @@
 require File.dirname(__FILE__) + '/masstransit/bus.rb'
 require File.dirname(__FILE__) + '/masstransit/subscription'
+require File.dirname(__FILE__) + '/masstransit/EndpointFactory'
---
+++ lib/masstransit/EndpointFactory.rb
@@ -0,0 +1,33 @@
+module MassTransit
+	class EndpointFactory
+	  @@factories = {}
+	  
+	  def get_type()
+		return ""
+	  end
+	  
+	  def EndpointFactory.add_factory(scheme, factory)
+		@@factories[scheme] = factory
+	  end
+	  
+	  def EndpointFactory.factories
+		return @@factories
+	  end
+	end
+
+	class LoopbackEndpointFactory < EndpointFactory
+	  def get_type()
+		return "Loopback"
+	  end
+	end
+
+	EndpointFactory.add_factory(:loopback, LoopbackEndpointFactory)
+
+	class RabbitMQEndpointFactory < EndpointFactory
+	  def get_type()
+		return "RabbitMQ"
+	  end
+	end
+
+	EndpointFactory.add_factory(:rabbitmq, RabbitMQEndpointFactory)
+end
