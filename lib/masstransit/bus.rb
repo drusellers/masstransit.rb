b597da2726100548afc5aa222cb9d743621d7aca
Travis Smith <travis@legomaster.net>
2010-07-08 17:17:12 -0400

    Starting building out the Bus enough we can test some more stuff.

--- lib/masstransit/bus.rb
+++ lib/masstransit/bus.rb
@@ -1,7 +1,13 @@
 module MassTransit
 	class Bus
+    attr_reader :addr
+
+    def initalize address
+      @addr = address
+    end
+
 	  def subscribe(message_name)
-	    
+      return Subscription.new @addr, message_name
     end
 	end
 end
